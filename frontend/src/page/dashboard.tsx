ffe663de51467b546fe929dbe0e2b4532d63d812
DKmen <dkgroupcoading@gmail.com>
2023-07-25 19:46:03 +0530

    change

--- frontend/src/page/dashboard.tsx
+++ frontend/src/page/dashboard.tsx
@@ -17,7 +17,7 @@ export default function DashboardPage() {
     <UserAuth>
       <div className="w-full flex flex-col h-screen">
         <div className="w-full p-4 bg-slate-500 flex flex-row items-center justify-between">
-          <span className="text-white text-2xl">{user.user.email}</span>
+          <span className="text-white text-2xl">""</span>
           <button
             className="border-none outline-none px-4 py-2 rounded bg-red-600 text-white text-lg"
             onClick={handleLogout}
